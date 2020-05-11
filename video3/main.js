74e9ba0927f3d5464e16e0872a8e1a23fb1ea8f0
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-05-11 17:27:34 +0200

    video3

--- video3/main.js
+++ video3/main.js
@@ -1,6 +1,16 @@
 new Vue({
-    el:'main',
-    data:{
-        mensaje:'hola mundo',
+    el: 'main',
+    data: {
+        laborales: ['Lunes', 'marter', 'miercoles', 'jueves', 'viernes'],
+        tareas: [
+            { nombre: "Hacer la compra", prioridad: 'baja' },
+            { nombre: "aprender Vue y firebase", prioridad: 'alta' },
+            { nombre: 'ir al gym', prioridad: 'alta' }
+        ],
+        persona: {
+            nombre: 'aaron',
+            profesion: 'developer',
+            ciudad: 'valencia'
+        }
     }
 });
