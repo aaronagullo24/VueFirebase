8976a917ded2fa6ed9d0c3817e78258440fad38b
aaron agullo sanchez <aaronagullo24@gmail.com>
2020-05-12 18:15:36 +0200

    video numero 10

---
+++ video10/main.js
@@ -0,0 +1,14 @@
+const wm1 = new Vue({
+    el: 'main',
+    data: {
+        mostrar: true,
+        mensaje: {
+            transicion: 'transiciones CSS',
+            animacion: 'Animacion CSS',
+            animacionCustom: 'Animacion Custom',
+            entreElementos: 'transicion entre elementos',
+        }
+    },
+});
+
+
